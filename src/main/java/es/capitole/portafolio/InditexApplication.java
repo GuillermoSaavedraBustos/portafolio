c6214ea28edcc7ea6df0887cad22c34ce617eff9
Guillermo Saavedra <gsaavedrab@bch.bancodechile.cl>
2021-10-11 19:02:47 -0300

    se generan carpetas iniciales, pom con dependencias y arquivo SQL para H2

---
+++ src/main/java/es/capitole/portafolio/InditexApplication.java
@@ -0,0 +1,11 @@
+package es.capitole.portafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InditexApplication.class, args);
+	}
+}
